cbe6ab11469f3966bb3eeb092ec71c2662b6a9dd
shong3 <42481531+shong3@users.noreply.github.com>
2019-04-01 16:07:24 -0500

    Trying to figure out errors

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,14 +6,15 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
   getQuizzes() {
+
     // Mocking up the data we will ultimately get from a 
     // REST endpoint, or web service call...
     return [
-      { name: 'Quiz 1', numberOfQuestions: 3}
-      , { name: 'Quiz 2', numberOfQuestions: 0}
-      , { name: 'Quiz 3', numberOfQuestions: 0}
-
+      { name: 'Quiz 1', numberOfQuestions: 3 }
+      , { name: 'Quiz 2', numberOfQuestions: 0 }
+      , { name: 'Quiz 3', numberOfQuestions: 0 }
     ];
   }
 }
