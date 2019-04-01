2da6372411545b2907e6bd3609927fbfc1107eff
Lucas Kostecki <lucas.kostecki@gmail.com>
2019-04-01 15:33:46 -0500

    Added class binding to list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
-  
+
   imageWidth = 200;
   get titleColor() {
     return this.imageWidth > 250 ? '#dd0031' : 'black';
+  }
+
+  get danger() {
+    return this.imageWidth > 250 ? true : false;
   }
 
   increaseWidth = () => {
