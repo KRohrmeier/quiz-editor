b954a55eabf7d4200c892189c24e27a6fcd1ac67
Kathrin Rohrmeier (Köhler) <theliteratecondition@gmail.com>
2019-04-01 15:15:47 -0500

    attribute property width ...
    ... getter for computed title color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
-  titleColor = "pink";
+  myWidth = 150;
+
+  get titleColor() {
+    return this.myWidth > 250 ? "pink" : "black";
+  }
+  increaseWidth = () => {
+    this.myWidth *= 1.5;
+  }
 }
