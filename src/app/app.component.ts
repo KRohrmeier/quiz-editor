76208cede044b6bc75b96014c7796d8615c904df
Lucas Kostecki <lucas.kostecki@gmail.com>
2019-04-01 15:50:30 -0500

    Generated quiz service for testing. Add constructor to the app component. Get quizzes method in service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+
+  constructor(private quizSvc: QuizService) {
+    console.log(this.quizSvc.getQuizzes());
+  }
 }
---
+++ src/app/quiz.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+
+  constructor() { }
+
+  // Mock up quiz service
+  getQuizzes() {
+    return [
+      {
+        name: 'Quiz 1',
+        numberofquestions: 3
+      },
+      {
+        name: 'Quiz 2',
+        numberofquestions: 8
+      },
+      {
+        name: 'Quiz 3',
+        numberofquestions: 5
+      }
+    ];
+  }
+}
