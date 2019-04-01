c824cb574419ce2e905cd503c9a1518cbe99381e
Belloche <dergiggler@gmail.com>
2019-04-01 16:01:48 -0500

    Added biz rule for red badges if no questions...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,17 @@ export class AppComponent {
 
   constructor(private quizSvc: QuizService) {
     
-    console.log(this.quizSvc.getQuizzes());
+    // console.log(this.quizSvc.getQuizzes());
+
+    this.quizzes = this.quizSvc.getQuizzes();
 
   }
 
   title = 'quiz-editor';
 
   myWidth = 250;
+
+  quizzes = [];
 
   get titleColor() {
     return this.myWidth > 250 ? "pink" : "black";
