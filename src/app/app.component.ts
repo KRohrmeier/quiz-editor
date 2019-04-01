2db63fdccb41fc6b7d4c07da54e1c48cfc0afe89
Belloche <dergiggler@gmail.com>
2019-04-01 15:50:06 -0500

    Generated QuizService and...
    ng generate service quiz
    
    Then added ctor automatic property to root component...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private quizSvc: QuizService) {
+    
+    console.log(this.quizSvc.getQuizzes());
+
+  }
+
   title = 'quiz-editor';
+
   myWidth = 250;
 
   get titleColor() {
