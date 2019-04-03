de8a558e387212a27a3fd4ffa56177c0db0dafc6
Michael Hall <47337730+mHall31@users.noreply.github.com>
2019-04-03 15:01:42 -0500

    Did to much before commits
    Added Two new cards to View, some unconnected buttonss, and an interface
    
    Tom's tallking about fixing interfaces in "goofball" ways with missnamed attributes
    seems like a cool fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { QuizService } from './quiz.service';
 
+interface QuizDisplay {
+  name: String;
+  numberOfQuestions: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,7 +22,7 @@ export class AppComponent {
 
   myWidth = 250;
 
-  quizzes = [];
+  quizzes: QuizDisplay[] = [];
 
   alterWidth = () => {
     this.myWidth *= 1.20;
