7b88495aae59807e67516fa40c7d8c8f8ca5d24a
Aseng Vue <35584537+asevue@users.noreply.github.com>
2019-04-03 15:07:18 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { QuizService } from './quiz.service';
 
+interface QuizDisplay {
+  name: string;
+  numberOfQuestions: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,25 +13,26 @@ import { QuizService } from './quiz.service';
 })
 export class AppComponent {
 
-  constructor( private quizSvc: QuizService) {
+  constructor(private quizSvc: QuizService) {
     //console.log(this.quizSvc.getQuizzes());
     this.quizzes = this.quizSvc.getQuizzes();
   }
+
   title = 'quiz-editor';
+  
   myWidth = 250;
 
-  quizzes = [];
-
-  //titleColor = this.myWidth > 250 ? "pink" : "black";
+  quizzes: QuizDisplay[] = [];
 
   get titleColor() {
     return this.myWidth > 250 ? "pink" : "black";
+  }  
+
+  increaseWidth = () => {
+    this.myWidth *= 1.5;
   }
 
-  increaseWidth = () => this.myWidth *= 1.5;
-
   get listBackgroundColorDanger() {
-    //return this.myWidth > 250 ? "bg-danger" : "";
     return this.myWidth > 250 ? true : false;
   }
 }
