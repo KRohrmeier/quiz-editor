fe594c46f6a734cee8fd9c9d147e9d3a9a244db1
Kathrin Rohrmeier (Köhler) <theliteratecondition@gmail.com>
2019-04-22 15:23:08 -0500

    get first animation working!(details from left)
    - import animations
    - add attribute bindings to html
    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
+import { animate } from '@angular/animations';
+import { transition } from '@angular/animations';
+import { trigger } from '@angular/animations';
+import { style } from '@angular/animations';
+import { keyframes } from '@angular/animations';
 
 interface QuizDisplay {
   name: string;
@@ -18,7 +23,29 @@ interface QuestionDisplay {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('detailsFromLeft', [
+      transition('leftPosition => finalPosition', [
+        animate('250ms', keyframes([
+          style({ left: '-30px', offset: 0.0 }),
+          style({ left: '-20px', offset: 0.25 }),
+          style({ left: '-10px', offset: 0.5 }),
+          style({ left: '-5px', offset: 0.75 }),
+          style({ left: '0px', offset: 1.0 })
+        ]))
+      ]),
+    ]),
+    trigger('pulseSaveCancelButtons', [
+      transition('nothingToSave => somethingToSave', [
+        animate('400ms', keyframes([
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 0.0 }),
+          style({ transform: 'scale(1.2)', 'transform-origin': 'top left', offset: 0.5 }),
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
 
@@ -58,6 +85,7 @@ export class AppComponent implements OnInit {
 
   setSelectedQuiz(q: QuizDisplay) {
     this.selectedQuiz = q;
+    this.detailsAnimationState = "finalPosition";
   }
 
   get titleColor() {
@@ -83,7 +111,8 @@ export class AppComponent implements OnInit {
     };
 
     this.quizzes = [...this.quizzes, newQuiz];
-    this.selectedQuiz = newQuiz;
+    this.setSelectedQuiz(newQuiz);  // so that the animation works with new quiz, too
+    // this.selectedQuiz = newQuiz;
   }
 
   removeQuestion(questionToDelete) {
@@ -116,6 +145,16 @@ export class AppComponent implements OnInit {
     return this.quizzes.filter(x => x.originalName === 'New Untitled Quiz').length;
   }
 
+  /* animation properties and methods */
+
+detailsAnimationState = "leftPosition";
+detailsFromLeftAnimationComplete() {
+  this.detailsAnimationState = "leftPosition";
+}
+
+
+
+/* learning promises */
   jsPromisesOne() {
     const x = this.quizSvc.getNumberPromise(true);
     console.log(x); // ? ? ? 
